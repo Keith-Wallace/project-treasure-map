e124bd3851db600dcb12387e4dec82d2e284a60e
Keith Wallace <keith.t.wallace@gmail.com>
2026-06-05 13:28:22 -0400

    add .env file for hidden API keys

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import dotenv from "dotenv";
+
+dotenv.config(); // Load .env file
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +11,8 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+
+  define: {
+    "process.env": process.env,
+  },
 })
